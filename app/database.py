2b876195831c222d22c3d01f35b8aa881c9b0b8c
masiweb <masiha.web@gmail.com>
2026-09-21 10:57:28 +0330

    chore: add app/database.py

---
+++ app/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from .config import get_settings
+
+engine = create_engine(get_settings().database_url, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
